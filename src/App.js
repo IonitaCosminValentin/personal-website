02f53f8475eb43465e5412aa2ceada94cb550f05
Ionita Cosmin Valentin <valy.18@yahoo.com>
2022-07-10 10:49:29 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-import "./App.css";
+import Intro from "./Components/Intro";
+import "./Css/App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Intro />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Components/Intro.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "../Css/Intro.css";
+
+const Intro = () => {
+  return (
+    <div className="Intro">
+      <h4>Welcome, </h4>
+
+      <h1>I'm Vali</h1>
+
+      <p>Junior Front-End Developer</p>
+    </div>
+  );
+};
+
+export default Intro;
